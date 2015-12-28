1f5d9df56b6c2f360fde3e950bf417e560d8462b
aniscom <a.elloumi92@gmail.com>
2015-12-28 15:55:49 +0100

    updateone

---
+++ api/services/TodoService.js
@@ -0,0 +1,23 @@
+/**
+ * Created by anis on 28/12/15.
+ */
+module.exports = {
+    getTodos: function(next) {
+        Todo.find().exec(function(err, todos) {
+            if(err) throw err;
+            next(todos);
+        });
+    },
+    addTodo: function(todoVal, next) {
+        Todo.create({value: todoVal}).exec(function(err, todo) {
+            if(err) throw err;
+            next(todo);
+        });
+    },
+    removeTodo: function(todoVal, next) {
+        Todo.destroy({value: todoVal}).exec(function(err, todo) {
+            if(err) throw err;
+            next(todo);
+        });
+    }
+};
